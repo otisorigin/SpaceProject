8c5a0b832651f407b9ffe4e020894d159c164630
fedor <fedorrimarovich@gmail.com>
2019-06-21 21:34:42 +0300

    started controller

---
+++ Assets/Game/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public float panSpeed = 0.20f;
+
+    public float panBorderThickness = 10f;
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
+        {
+            pos.y += panSpeed * Time.deltaTime;
+        }
+
+        transform.position = pos;
+
+    }
+}
